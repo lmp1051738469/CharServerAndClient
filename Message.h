9820b6c6fa218ade4248d0d831a78435dbbc71ca
lmp1051738469 <1051738469@qq.com>
2023-10-11 21:18:10 +0800

    测试1

--- Message.h
+++ Message.h
@@ -1,1 +1,16 @@
 #include<string>
+
+class Message
+{
+private:
+    int id;
+    std::string msg;
+public:
+    Message() = default;
+    Message(int id_ , std::string msg_) : id(id_) , msg(msg_){}
+    ~Message() = default;
+    const char* str_c()const {return msg.c_str()};
+    int get_id()const {return id;}
+};
+
+
