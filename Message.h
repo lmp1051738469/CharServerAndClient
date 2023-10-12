9c238bc128dfeff12878de6eefd4e3df13d4e06a
lmp1051738469 <1051738469@qq.com>
2023-10-12 09:06:37 +0800

    更改Message的位置

--- Message.h
+++ Message.h
@@ -1,16 +1,40 @@
+#include <boost/archive/text_oarchive.hpp> 
+#include <boost/archive/text_iarchive.hpp> 
+#include <boost/serialization/string.hpp> 
 #include<string>
-//add message  proto
+#include<sstream>
+
+static std::stringstream ss;
+
 class Message
 {
 private:
+friend class boost::serialization::access; 
     int id;
-    std::string msg;
+    std::string content;
+  template <typename Archive> 
+  friend void serialize(Archive &ar, Message &p, const unsigned int version)
+  {
+    ar & p.id; 
+    ar & p.content; 
+  }
+
 public:
     Message() = default;
-    Message(int id_ , std::string msg_) : id(id_) , msg(msg_){}
+    Message(int id_ , std::string msg_) : id(id_) , content(msg_){}
     ~Message() = default;
-    const char* str_c()const {return msg.c_str()};
+    const char* str_c()const {return content.c_str();};
     int get_id()const {return id;}
+    void save_msg()
+    {
+        boost::archive::text_oarchive oa(ss); 
+        oa << *this; 
+    }
+    void load_msg()
+    {
+        boost::archive::text_iarchive ia(ss);  
+        ia >> *this; 
+    }
 };
 
 
